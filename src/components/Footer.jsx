dce42c62c917442fa611c84c4ca676b2d472f9d0
MathisP <shiray@hotmail.fr>
2023-10-28 03:34:48 +0200

    ok

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import "../css/footer.scss";
 
 export default function Footer() {
-  return <footer>test</footer>;
+  return <footer>Ceci est l'emplacement du footer</footer>;
 }
